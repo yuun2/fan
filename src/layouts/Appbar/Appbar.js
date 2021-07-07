bef95b9d39919e614f71eefeb9a066b9cad573c6
yuun2 <86525699+yuun2@users.noreply.github.com>
2021-07-07 18:59:17 +0900

    Revert "revise appbar"

--- src/layouts/Appbar/Appbar.js
+++ src/layouts/Appbar/Appbar.js
@@ -5,20 +5,18 @@ import Container from '@material-ui/core/Container';
 import img1 from "../../images/mark.jpg"
 import img2 from "../../images/knu.jpg"
 
-import useStyles from "./appbarStyles";
-
-export default function Appbar() {
-  const classes = useStyles();
-
+const Appbar = () => {
   return (
-    <AppBar position="static" className={classes.root}>
-      <Toolbar className={classes.toolbar}>
-        <img src={img1} width="50" height="47"/>
-        <img src={img2} width="120" height="35" hspace="18" vspace="7"/>
-        <Typography className={classes.title}>
-          창업 오픈스페이스
-        </Typography>
-      </Toolbar>
-    </AppBar>
+      <AppBar position="relative" color="red">
+        <Toolbar>
+          <img src={img1} width="45" height="43"/>
+          <img src={img2} width="80" height="25" hspace="10"/>
+          <Typography variant="h6" noWrap>
+            창업 오픈스페이스
+          </Typography>
+        </Toolbar>
+      </AppBar>
   );
-}
+};
+
+export default Appbar;
